a420bf434b16f44333595fe7530709fe2f52c290
HadasHadad <123874368+HadasHadad@users.noreply.github.com>
2025-05-04 18:19:54 +0300

    Update gptMock.js
    added a word to badWords array

--- project/gptMock.js
+++ project/gptMock.js
@@ -1,5 +1,5 @@
 function isOffensive(message) {
-    const badWords = ["טיפש", "אידיוט", "מטומטם", "fuck"];
+    const badWords = ["טיפש", "אידיוט", "מטומטם", "fuck", "אוטיסט"];
     return badWords.some(word => message.includes(word));
   }
   
