3aea9d69fcd36964ed3b8b9ddf94fc3b3af35459
titiMekonan <strmqwnn6@gmail.com>
2025-05-08 19:41:26 +0300

    added nav bar

---
+++ project/nav.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const nav = document.getElementById("main-nav");
+    const loggedIn = localStorage.getItem("loggedIn");
+  
+    if (nav) {
+      if (loggedIn === "true") {
+        nav.innerHTML = `
+          <a href="index.html">בית</a>
+          <a href="the-game.html">The Game</a>
+          <a href="about.html">אודות</a>
+          <a href="profile.html">פרופיל</a>
+          <a href="#" onclick="logoutUser()">התנתק</a>
+        `;
+      } else {
+        nav.innerHTML = `
+          <a href="index.html">בית</a>
+          <a href="the-game.html">The Game</a>
+          <a href="about.html">אודות</a>
+          <a href="login.html">התחברות</a>
+        `;
+      }
+    }
+  });
+  
