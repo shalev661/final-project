fce305da28bdaf301d29b8fac751d12dc2d2d1f9
Micolpenkar <penkarmicol@gmail.com>
2025-05-11 18:03:28 +0300

    added a bad word

--- project/gptMock.js
+++ project/gptMock.js
@@ -1,5 +1,5 @@
 function isOffensive(message ) {
-    const badWords = ["טיפש", "אידיוט", "מטומטם", "fuck", "fuckyou"];
+    const badWords = ["טיפש", "אידיוט", "מטומטם", "fuck", "fuckyou" , "go to hell" , "בן זונה" , "זונה" , "בת זונה" , "שרמוטה"]; 
     return badWords.some(word => message.includes(word));
   }
   
