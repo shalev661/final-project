c5f2d0a6686ff8640a908d447a1df768eb3f2961
michal456789 <michalit13012009@gmail.com>
2025-05-11 17:52:57 +0300

    add word

--- project/gptMock.js
+++ project/gptMock.js
@@ -1,5 +1,5 @@
 function isOffensive(message ) {
-    const badWords = ["טיפש", "אידיוט", "מטומטם", "fuck", "fuckyou"];
+    const badWords = ["טיפש", "אידיוט", "מטומטם", "fuck", "fuckyou","חסר בורג"];
     return badWords.some(word => message.includes(word));
   }
   
