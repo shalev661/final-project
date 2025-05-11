f36346a3d38e375fa1055d8703207803d282a938
dabibi123 <ywntnmgydys@gmail.com>
2025-05-11 18:50:07 +0300

    i have built the css of the sign up page.

--- project/gptMock.js
+++ project/gptMock.js
@@ -1,5 +1,5 @@
 function isOffensive(message ) {
-    const badWords = ["טיפש", "אידיוט", "מטומטם", "fuck", "fuckyou"];
+    const badWords = ["טיפש", "אידיוט", "מטומטם", "fuck", "fuckyou","idiot"];
     return badWords.some(word => message.includes(word));
   }
   
