24db16c9a590a6994d752f4727c9e4982206054f
shalev661 <shalev7668@rogozin.net>
2025-05-22 20:07:52 +0300

    Arrangement of the website display

---
+++ project/data.js
@@ -0,0 +1,37 @@
+const players = [
+    { id: 1, name: "שחקן1", password: "password1", reputation: 10 },
+    { id: 2, name: "שחקן2", password: "password2", reputation: 10 },
+    { id: 3, name: "שחקן3", password: "password3", reputation: 10 },
+];
+
+let inputarea = document.getElementById("inputarea");
+let inputarea1 = document.getElementById("inputarea1");
+let cnfrmbtn = document.getElementById("cnfrmbtn");
+
+
+
+
+let AddToPlayerBaseBtn = document.getElementById("AddToPlayerBase");
+AddToPlayerBaseBtn.disabled = true;
+
+let IdUser = players.length + 1;
+console.log(IdUser);
+
+let username;
+let password1;
+
+function Registration() {
+    username = inputarea.value;
+    password1 = inputarea1.value;
+    AddToPlayerBaseBtn.disabled = false;
+
+}
+
+cnfrmbtn.addEventListener("click", () => Registration());
+
+function AddToPlayerBase(username, password1) {
+    players.push({ id: IdUser, name: username, password: password1, reputation: 10 });
+
+}
+
+AddToPlayerBaseBtn.addEventListener("click", () => AddToPlayerBase(username, password1));
---
+++ project/nav.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const nav = document.getElementById("main-nav");
+    const loggedIn = localStorage.getItem("loggedIn");
+  
+  //   if (nav) {
+  //     if (loggedIn === "true") {
+  //       nav.innerHTML = `
+  //         <a href="index.html">בית</a>
+  //         <a href="game.html" >🎮המשחק </a>
+  //         <a href="profile.html">פרופיל שחקן</a>
+  //         <a href="#" onclick="logoutUser()">התנתק</a>
+  //       `;
+  //     } else {
+  //       nav.innerHTML = `
+  //         <a href="index.html">בית</a>
+  //         <a href="game.html"> 🎮המשחק</a>
+  //         <a href="Sign-Up_Page.html">התחברות</a>
+  //         <a href="Sign-Up_Page.html">הרשמה</a>
+  //       `;
+  //     }
+  //   }
+  });
+  
