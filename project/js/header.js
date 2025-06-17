32842ffd588b75af9c5f8ae745150bbd7d6c0551
shalev661 <shalev7668@rogozin.net>
2025-06-17 19:59:50 +0300

    errors header and footer

--- project/js/header.js
+++ project/js/header.js
@@ -1,26 +1,16 @@
 function header(){
   document.addEventListener("DOMContentLoaded", () => {
     const headerPlace = document.getElementById("headerplace");
+    const title = document.getElementById("title")
+    headerPlace.innerHTML = `
+          <a href="#">בית</a>
+          <a href="./project/game.html"> 🎮המשחק</a>
+          <a href="./project/about.html">אודות</a>
+          <a href="./project/log-in_age.html">התחברות</a>
+          <a href="./project/Sign-Up_Page.html">הרשמה</a>
+          <a href="./project/Contactandhelp.html">צור קשר</a>
+        `;
+      }
+)}
 
-     fetch('./project/header.html')
-      .then(response => response.text())
-      .then(data => {
-        if (headerPlace) {
-          headerPlace.innerHTML = data;
-        }
-        const nav = document.getElementById("main-nav");
-        const loggedIn = localStorage.getItem("loggedIn");
-
-       
-      });
-
-      
-  });
-}
-
-
-
-
-header()    
-
-  
+header()
