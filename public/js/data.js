8be3e6cf4155f0f80089c864f001e0de0512cf2b
dabibi123 <ywntnmgydys@gmail.com>
2025-06-19 20:02:29 +0300

    profile in progress בע"זה will work on יום ראשון

--- public/js/data.js
+++ public/js/data.js
@@ -1,8 +1,9 @@
 
 let players = JSON.parse(localStorage.getItem("players")) || [
-  { id: 1, name: "שחקן1", password: "password1", reputation: 10 },
-  { id: 2, name: "שחקן2", password: "password2", reputation: 0 },
+  { id: 1, name: "שחקן1", password: "password1", reputation: 10,gameWon: gameswon1,gameLost: gameslost1 },
+  { id: 2, name: "שחקן2", password: "password2", reputation: 0,gameWon: gameswon1,gameLost: gameslost1 },
   { id: 3, name: "שחקן3", password: "password3", reputation: 10 },
+
 ];
 
 let inputarea = document.getElementById("inputarea");
@@ -32,7 +33,7 @@ function AddToPlayerBase(username, password1) {
 
   const IdUser = players.length ? players[players.length - 1].id + 1 : 1;
 
-  const newPlayer = { id: IdUser, name: username, password: password1, reputation: 10 };
+  const newPlayer = { id: IdUser, name: username, password: password1, reputation: 10, gameLost: gameslost1};
   players.push(newPlayer);
 
 
