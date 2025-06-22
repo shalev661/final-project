eb9a45eb7eeb35dbb5cd27f964d6233191370a7c
shalev661 <shalev7668@rogozin.net>
2025-06-22 18:44:27 +0300

    fix header

--- public/js/header.js
+++ public/js/header.js
@@ -1,26 +1,20 @@
-function header(){
+function header() {
   document.addEventListener("DOMContentLoaded", () => {
     const headerPlace = document.getElementById("headerplace");
-
-     fetch('./project/header.html')
-      .then(response => response.text())
-      .then(data => {
-        if (headerPlace) {
-          headerPlace.innerHTML = data;
-        }
-        const nav = document.getElementById("main-nav");
-        const loggedIn = localStorage.getItem("loggedIn");
-
-       
-      });
-
-      
-  });
+    const img = document.createElement("img")
+    img.src = "./public/img/img-header.jpg"
+    img.alt = "..."
+    const img2 = document.getElementById("img-get")
+    img2.appendChild(img)
+    headerPlace.innerHTML = `
+          <a href="#">בית</a>
+          <a href="../public/game.html">🎮המשחק</a>
+          <a href="./public/about.html">אודות</a>
+          <a href="../public/log-in_page.html">התחברות</a>
+          <a href="./public/Sign-Up_Page.html">הרשמה</a>
+          <a href="./public/Contactandhelp.html">צור קשר</a>
+        `;
+  })
 }
 
-
-
-
 header()    
-
-  
