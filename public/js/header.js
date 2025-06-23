9f1159cbe4dea8eb21e30a9d34a525148dd2f577
shalev661 <shalev7668@rogozin.net>
2025-06-23 21:09:45 +0300

    improve: header+footer

--- public/js/header.js
+++ public/js/header.js
@@ -1,20 +1,25 @@
 function header() {
   document.addEventListener("DOMContentLoaded", () => {
-    const headerPlace = document.getElementById("headerplace");
-    const img = document.createElement("img")
-    img.src = "./public/img/img-header.jpg"
-    img.alt = "..."
+    const headerPlace = document.getElementById("headerplace")
     const img2 = document.getElementById("img-get")
-    img2.appendChild(img)
     headerPlace.innerHTML = `
+    <a id="title2" href="../public/index.html">VerbaGuard</a><br><br><br><br>
+    <div id="a-header">
           <a href="#">בית</a>
           <a href="../public/game.html">🎮המשחק</a>
           <a href="../public/about.html">אודות</a>
           <a href="../public/log-in_page.html">התחברות</a>
           <a href="../public/Sign-Up_Page.html">הרשמה</a>
           <a href="../public/Contactandhelp.html">צור קשר</a>
+          </div>
         `;
+    const img = document.createElement("img")
+    img.src = "../final-project2/public/img/img-header.jpg"
+    img.alt = "..."
+    img.style.width = "150px";
+    img.style.height = "150px";
+    img2.appendChild(img)
   })
 }
 
-header()    
+header()
