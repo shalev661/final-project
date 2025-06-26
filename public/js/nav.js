57a5025d1e65ba891197ac77fe9bfe093f5e6937
titiMekonan <strmqwnn6@gmail.com>
2025-06-26 19:03:28 +0300

    I sorted the links.

--- public/js/nav.js
+++ public/js/nav.js
@@ -6,17 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
     if (loggedIn === "true") {
       nav.innerHTML = `
         <a href="/index.html">בית</a>
-        <a href=".//game.html" >🎮המשחק </a>
+        <a href="/public/game.html" >🎮המשחק </a>
         <a href="/public/profile.html">פרופיל שחקן</a>
         <a href="/public/contactandhelp.html">צור קשר</a>
-        <a href="./about.html">אודות</a>
+        <a href="/public/about.html">אודות</a>
         <a href="#" onclick="logoutUser()">התנתק</a>
       `;
     } else {
       nav.innerHTML = `
         <a href="/index.html">בית</a>
-        <a href="./game.html">🎮המשחק</a>
-        <a href="./about.html">אודות</a>
+        <a href="/public/game.html">🎮המשחק</a>
+        <a href="/public/about.html">אודות</a>
         <a href="/public/Log-in_Page.html">התחברות</a>
         <a href="/public/Sign-Up_Page.html">הרשמה</a>
         <a href="/public/contactandhelp.html">צור קשר</a>
