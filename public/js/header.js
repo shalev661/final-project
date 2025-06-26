a9c0d43a7cb50404ef688bfa767e8f156468dc18
shalev661 <shalev7668@rogozin.net>
2025-06-26 18:10:01 +0300

    header

--- public/js/header.js
+++ public/js/header.js
@@ -1,23 +1,15 @@
 function header() {
   document.addEventListener("DOMContentLoaded", () => {
     const headerPlace = document.getElementById("headerplace");
-    const img = document.createElement("img")
-    img.src = "./img/img-header.jpg"
+        const img = document.getElementById("img-get");
+    img.src = "./public/img/img-header.jpg"
     img.alt = "..."
         img.style.width = "150px";
     img.style.height = "150px";
-    img2.appendChild(img)
-    const img2 = document.getElementById("img-get")
     headerPlace.innerHTML = `
-    <a id="title2" href="../public/index.html">VerbaGuard</a><br><br><br><br>
+    <a id="title2" href="../public/index.html">VerbaGuard</a>
     <div id="a-header">
-          <a href="#">בית</a>
-          <a href="../public/game.html">🎮המשחק</a>
-          <a href="../public/about.html">אודות</a>
-          <a href="../public/log-in_page.html">התחברות</a>
-          <a href="../public/Sign-Up_Page.html">הרשמה</a>
-          <a href="../public/Contactandhelp.html">צור קשר</a>
-          </div>
+  <nav id="main-nav"></nav>
         `;
   })
 }
